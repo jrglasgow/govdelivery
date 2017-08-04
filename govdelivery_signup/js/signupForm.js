3b7eea73d3cfc1583612b66b473eea07dce8d24e
James Glasgow <james.glasgow@eyestreet.com>
2017-08-04 08:46:52 -0600

    removing some debugging code and fixing javascript issues with event.stopPropagation();

--- govdelivery_signup/js/signupForm.js
+++ govdelivery_signup/js/signupForm.js
@@ -1,17 +1,20 @@
+/**
+ * @file
+ * handle form submission. open up the new page in a new tab, this will prevent
+ * the user from leaving the site.
+ */
 (function ($) {
-
 
 Drupal.behaviors.govDeliverySignup = {
   'attach': function() {
-    $('form#govdelivery-signup .button.form-submit:not(.gdsf-processed)')
+    $('form.govdelivery-signup')
     .addClass('gdsf-processed')
-    .click(function(event) {
+    .submit(function(event) {
       if ($('form#govdelivery-signup #edit-email').val() !== '') {
         var email = $('form#govdelivery-signup #edit-email').val();
         var url = drupalSettings.govDeliverySignup.url + 'email=' + encodeURIComponent(email);
-        console.log(url);
+        event.preventDefault();
         window.open(url, '_blank');
-        event.stopPropagation();
       }
     });
   },
